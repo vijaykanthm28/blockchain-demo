6b006b9a656631b3f5dc11e830be1a4b0b7447ae
vijaykanthm28 <mvijaykanth@megam.io>
2018-05-16 18:25:41 +0530

    build file changes

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "******** build start ******"
 echo "pwd:"
+ls /var/lib/rioos/temp/blockchain-demo-1/upload/src
+ls -la /
+ls -la /tmp
+ls -la /var/lib/rioos/temp/blockchain-demo-1/upload/src
 pwd
 npm install 
 
